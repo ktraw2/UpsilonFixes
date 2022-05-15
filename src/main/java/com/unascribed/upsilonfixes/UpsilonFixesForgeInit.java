fa1151a2d00cc41733f101728bc6c300849ab82d
Una Thompson <me@unascribed.com>
2022-05-14 20:07:29 -0700

    Rework logo draw patch

--- src/main/java/com/unascribed/upsilonfixes/UpsilonFixesForgeInit.java
+++ src/main/java/com/unascribed/upsilonfixes/UpsilonFixesForgeInit.java
@@ -1,38 +1,10 @@
 package com.unascribed.upsilonfixes;
 
-import com.unascribed.upsilonfixes.branding.GuiMainMenuVoxelBoxTransformer;
-
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL12.*;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import javax.imageio.ImageIO;
-
-import org.lwjgl.BufferUtils;
-
 
 public class UpsilonFixesForgeInit implements Runnable {
 
 	@Override
 	public void run() {
-		if (UpsilonFixesConfig.enableUpsilonBranding) {
-			GuiMainMenuVoxelBoxTransformer.textureId = glGenTextures();
-			try {
-				glBindTexture(GL_TEXTURE_2D, GuiMainMenuVoxelBoxTransformer.textureId);
-				BufferedImage img = ImageIO.read(getClass().getResource("/upsilon-logo.png"));
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-				ByteBuffer buf = BufferUtils.createByteBuffer(img.getWidth()*img.getHeight()*4);
-				int[] arr = new int[img.getWidth()*img.getHeight()];
-				img.getRGB(0, 0, img.getWidth(), img.getHeight(), arr, 0, img.getWidth());
-				buf.asIntBuffer().put(arr);
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.getWidth(), img.getHeight(), 0, GL_BGRA, GL_UNSIGNED_BYTE, buf);
-			} catch (IOException e) {
-				UpsilonFixesPremain.log.error("Failed to load logo texture", e);
-			}
-		}
 	}
 	
 }
--- src/main/java/com/unascribed/upsilonfixes/branding/GuiMainMenuVoxelBoxTransformer.java
+++ src/main/java/com/unascribed/upsilonfixes/branding/GuiMainMenuVoxelBoxTransformer.java
@@ -1,5 +1,20 @@
 package com.unascribed.upsilonfixes.branding;
 
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import javax.imageio.ImageIO;
+
+import org.lwjgl.BufferUtils;
+
+import com.unascribed.upsilonfixes.UpsilonFixesPremain;
+
+import net.minecraft.src.GuiMainMenu;
+import net.minecraft.src.Tessellator;
 import nilloader.api.lib.mini.MiniTransformer;
 import nilloader.api.lib.mini.PatchContext;
 import nilloader.api.lib.mini.annotation.Patch;
@@ -7,85 +22,55 @@ import nilloader.api.lib.mini.annotation.Patch;
 @Patch.Class("com.thevoxelbox.voxelmenu.GuiMainMenuVoxelBox")
 public class GuiMainMenuVoxelBoxTransformer extends MiniTransformer {
 
-	public static int textureId;
-	
 	@Patch.Method("a(IIF)V")
 	public void patchDrawScreen(PatchContext ctx) {
-		int y = 3;
-		int logoWidth = 160;
-		int logoHeight = 23;
-
 		ctx.jumpToLastReturn();
 		ctx.add(
-				GETSTATIC("org/lwjgl/opengl/GL11", "GL_TEXTURE_2D", "I"),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glEnable", "(I)V"),
-				GETSTATIC("org/lwjgl/opengl/GL11", "GL_CULL_FACE", "I"),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glDisable", "(I)V"),
-				
-				GETSTATIC("org/lwjgl/opengl/GL11", "GL_TEXTURE_2D", "I"),
-				LDC(textureId),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glBindTexture", "(II)V"),
-
-				LDC(1f),
-				LDC(1f),
-				LDC(1f),
-				LDC(1f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glColor4f", "(FFFF)V"),
-
-				ALOAD(0),
-				GETFIELD("net/minecraft/src/GuiMainMenu", "width", "I"),
-				LDC(logoWidth),
-				ISUB(),
-				LDC(2),
-				IDIV(),
-				I2F(),
-				FSTORE(3),
-
-				GETSTATIC("org/lwjgl/opengl/GL11", "GL_QUADS", "I"),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glBegin", "(I)V"),
-
-				LDC(0f),
-				LDC(0f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glTexCoord2f", "(FF)V"),
-
-				FLOAD(3),
-				LDC((float)y),
-				LDC(0f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glVertex3f", "(FFF)V"),
-
-				LDC(1f),
-				LDC(0f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glTexCoord2f", "(FF)V"),
-
-				FLOAD(3),
-				LDC((float)logoWidth),
-				FADD(),
-				LDC((float)y),
-				LDC(0f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glVertex3f", "(FFF)V"),
-
-				LDC(1f),
-				LDC(1f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glTexCoord2f", "(FF)V"),
-
-				FLOAD(3),
-				LDC((float)logoWidth),
-				FADD(),
-				LDC((float)(y+logoHeight)),
-				LDC(0f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glVertex3f", "(FFF)V"),
-
-				LDC(0f),
-				LDC(1f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glTexCoord2f", "(FF)V"),
-
-				FLOAD(3),
-				LDC((float)(y+logoHeight)),
-				LDC(0f),
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glVertex3f", "(FFF)V"),
-
-				INVOKESTATIC("org/lwjgl/opengl/GL11", "glEnd", "()V")
-				);
+			ALOAD(0),
+			INVOKESTATIC("com/unascribed/upsilonfixes/branding/GuiMainMenuVoxelBoxTransformer$Hooks", "drawLogo", "(Lnet/minecraft/src/GuiMainMenu;)V")
+		);
+	}
+	
+	public static class Hooks {
+		
+		private static int textureId = -1;
+		
+		public static void drawLogo(GuiMainMenu gui) {
+			if (textureId == -1) {
+				textureId = glGenTextures();
+				try {
+					glBindTexture(GL_TEXTURE_2D, textureId);
+					BufferedImage img = ImageIO.read(Hooks.class.getResource("/upsilon-logo.png"));
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+					ByteBuffer buf = BufferUtils.createByteBuffer(img.getWidth()*img.getHeight()*4);
+					int[] arr = new int[img.getWidth()*img.getHeight()];
+					img.getRGB(0, 0, img.getWidth(), img.getHeight(), arr, 0, img.getWidth());
+					buf.asIntBuffer().put(arr);
+					glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.getWidth(), img.getHeight(), 0, GL_BGRA, GL_UNSIGNED_BYTE, buf);
+				} catch (IOException e) {
+					UpsilonFixesPremain.log.error("Failed to load logo texture", e);
+				}
+			}
+			glEnable(GL_TEXTURE_2D);
+			glColor3f(1, 1, 1);
+			int w = 160;
+			int h = 23;
+			
+			int x = (gui.width-w)/2;
+			int y = 3;
+			
+			glBindTexture(GL_TEXTURE_2D, textureId);
+			Tessellator tess = Tessellator.instance;
+			tess.startDrawingQuads();
+			//                   x    y    z    u  v
+			tess.addVertexWithUV(x  , y+h, 0,   0, 1);
+			tess.addVertexWithUV(x+w, y+h, 0,   1, 1);
+			tess.addVertexWithUV(x+w, y  , 0,   1, 0);
+			tess.addVertexWithUV(x  , y  , 0,   0, 0);
+			tess.draw();
+		}
+		
 	}
 
 }
