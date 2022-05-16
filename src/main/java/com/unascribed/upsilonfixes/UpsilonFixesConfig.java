cc83463839af2aeba9e4fa396cbaf6c78f483beb
Una Thompson <me@unascribed.com>
2022-05-15 19:52:20 -0700

    Switch to CSS config

--- src/main/java/com/unascribed/upsilonfixes/UpsilonFixesConfig.java
+++ src/main/java/com/unascribed/upsilonfixes/UpsilonFixesConfig.java
@@ -1,7 +1,6 @@
 package com.unascribed.upsilonfixes;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,7 +9,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
-import java.util.Properties;
+import java.util.Optional;
+
+import nilloader.api.lib.qdcss.QDCSS;
 
 public class UpsilonFixesConfig {
 
@@ -20,53 +21,82 @@ public class UpsilonFixesConfig {
 		String value();
 	}
 	
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.FIELD)
+	private @interface Key {
+		String value();
+	}
+	
+	@Key("upsilon-branding")
 	@Comment("Enables the Rewind Upsilon modpack branding.")
 	public static boolean enableUpsilonBranding = true;
+	
+	@Key("whirlpool-fix")
 	@Comment("Backports the 1.5 'water source blocks fill in above water' fix.")
 	public static boolean enableWhirlpoolFix = true;
-	@Comment("Fixes the attacker yaw not syncing from server to client, preventing the camera tilt animation from working when damaged.")
+	
+	@Key("attacker-yaw-syncing")
+	@Comment("Fixes the attacker yaw not syncing from server to client, preventing the camera tilt\nanimation from working when damaged.")
 	public static boolean enableAttackerYawSyncing = true;
-	@Comment("Prevents XyCraft's quartz crystals from generating. They're a huge performance hit and nobody likes them.")
+	
+	@Key("disable-xycraft-quartz-crystal-worldgen")
+	@Comment("Prevents XyCraft's quartz crystals from generating. They're a huge performance hit and\nnobody likes them.")
 	public static boolean disableXycraftQuartzCrystalWorldgen = true;
+	
+	@Key("fix-portal-gun-resources")
 	@Comment("Fixes sounds and music for Portal Gun by replacing the asset index.")
 	public static boolean enablePortalGunResourcesFix = true;
-	@Comment("Fixes transmutation crafting recipes in EE3 only working once, and being broken by NEI.")
+	
+	@Key("fix-ee3-transmute-recipes")
+	@Comment("Fixes transmutation crafting recipes in EE3 only working once, and being broken by\nNEI.")
 	public static boolean enableEE3TransmuteRecipesFix = true;
+	
+	@Key("fix-miscperipherals-asm")
 	@Comment("Fixes misuse of the ASM API by MiscPeripherals breaking with newer ASM libraries.")
 	public static boolean enableMiscPeripheralsAsmFix = true;
+	
+	@Key("fix-liteloader-log")
 	@Comment("Attaches the LiteLoader logger to the FML logger, making it look less ugly.")
 	public static boolean enableLiteLoaderLogFix = true;
-	@Comment("1.4.7 mods don't really get updates anymore, and many version checkers try to contact dead servers.")
+	
+	@Key("remove-version-checkers")
+	@Comment("1.4.7 mods don't really get updates anymore, and many version checkers try to contact\ndead servers.")
 	public static boolean disableVersionCheckers = true;
+	
+	@Key("remove-dead-cosmetics")
 	@Comment("Some mods add cosmetics (usually capes) that try to contact dead servers.")
 	public static boolean disableDeadCosmetics = true;
 	
 	static {
-		Properties props = new Properties();
-		File cfg = new File("config/upsilonfixes.ini");
-		try (FileInputStream fis = new FileInputStream(cfg)) {
-			props.load(fis);
+		File cfg = new File("config/upsilonfixes.css");
+		QDCSS css = QDCSS.load("", "");
+		try {
+			css = QDCSS.load(cfg);
 		} catch (FileNotFoundException e) {
 		} catch (IOException e) {
-			e.printStackTrace();
-			System.err.println("Failed to load UpsilonFixes config");
+			UpsilonFixesPremain.log.error("Failed to load upsilonfixes.css", e);
 		}
-		StringBuilder out = new StringBuilder("# UpsilonFixes configuration file\r\n# Any unrecognized keys or comments you add will be lost!\r\n\r\n\r\n");
+		System.out.println(css);
+		StringBuilder out = new StringBuilder("/*\r\n * UpsilonFixes configuration file\r\n * Any unrecognized keys or comments you add will be lost!\r\n */\r\n\r\nfeatures {\r\n");
 		try {
 			for (Field f : UpsilonFixesConfig.class.getDeclaredFields()) {
-				String k = f.getName();
-				if (!props.containsKey(k)) {
-					props.setProperty(k, Boolean.toString(f.getBoolean(null)));
+				String k = f.getAnnotation(Key.class).value();
+				Optional<Boolean> opt = css.getBoolean("features."+k);
+				boolean v;
+				if (opt.isPresent()) {
+					v = opt.get();
+					f.set(null, v);
 				} else {
-					f.set(null, Boolean.parseBoolean(props.getProperty(k)));
+					v = f.getBoolean(null);
 				}
 				Comment comment = f.getAnnotation(Comment.class);
-				if (comment != null) out.append("# "+comment.value()+"\r\n");
-				out.append(k+"="+Boolean.toString(f.getBoolean(null))+"\r\n\r\n");
+				if (comment != null) out.append("\t/*\r\n\t * "+comment.value().replace("\n", "\r\n\t * ")+"\r\n\t */\r\n");
+				out.append("\t"+k+": "+(v ? "on" : "off")+";\r\n\r\n");
 			}
 		} catch (IllegalAccessException e) {
 			throw new AssertionError(e);
 		}
+		out.append("}\r\n");
 		try (FileOutputStream fos = new FileOutputStream(cfg)) {
 			cfg.getParentFile().mkdirs();
 			fos.write(out.toString().getBytes("UTF-8"));
