1df2f86feec9325d7f0016f0175f11d5e3fbfae5
Una Thompson <me@unascribed.com>
2023-06-17 01:38:28 -0700

    Stop menu music when a world is loaded

--- src/main/java/com/rewindmc/upsilonfixes/redesign/MinecraftTransformer.java
+++ src/main/java/com/rewindmc/upsilonfixes/redesign/MinecraftTransformer.java
@@ -5,18 +5,19 @@ import com.rewindmc.upsilonfixes.UpsilonFixesConfig;
 import com.rewindmc.upsilonfixes.UpsilonMiniTransformer;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.SoundManager;
 import net.minecraft.client.gui.Screen;
 import net.minecraft.client.gui.ScreenIngameMenu;
 import net.minecraft.client.gui.ScreenMainMenu;
 import nilloader.api.lib.mini.PatchContext;
 import nilloader.api.lib.mini.annotation.Patch;
+import paulscode.sound.SoundSystem;
 
 @Patch.Class("net.minecraft.client.Minecraft")
 @ConfigOptions("redesignMenus")
 public class MinecraftTransformer extends UpsilonMiniTransformer {
 	
 	@Patch.Method("displayScreen(Lnet/minecraft/client/gui/Screen;)V")
-	@Patch.Method.AffectsControlFlow
 	public void patchDisplayScreen(PatchContext ctx) {
 		if (UpsilonFixesConfig.redesignMenus) {
 			ctx.jumpToStart();
@@ -28,7 +29,19 @@ public class MinecraftTransformer extends UpsilonMiniTransformer {
 		}
 	}
 	
+	@Patch.Method("runTick()V")
+	public void patchRunTick(PatchContext ctx) {
+		if (UpsilonFixesConfig.redesignMenus) {
+			ctx.jumpToStart();
+			ctx.add(
+				INVOKESTATIC(hooks(), "tick", "()V")
+			);
+		}
+	}
+	
 	public static class Hooks {
+		
+		private static boolean wasPlayingMusic = false;
 		
 		public static Screen replaceScreen(Screen orig) {
 			if ((orig instanceof ScreenMainMenu || (orig == null && Minecraft.instance().world == null)) && !(orig instanceof ScreenMainMenuUpsilon)) {
@@ -38,6 +51,20 @@ public class MinecraftTransformer extends UpsilonMiniTransformer {
 				return new ScreenIngameMenuUpsilon();
 			}
 			return orig;
+		}
+		
+		public static void tick() {
+			SoundSystem sys = SoundManager.sndSystem;
+			if (sys.playing("BgMusic")) {
+				if (Minecraft.instance.world != null && wasPlayingMusic) {
+					sys.stop("BgMusic");
+					wasPlayingMusic = false;
+				} else {
+					wasPlayingMusic = true;
+				}
+			} else {
+				wasPlayingMusic = false;
+			}
 		}
 		
 	}
